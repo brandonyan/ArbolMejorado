ae730ef188ebc74b6e57b29667e3a9edab88cc40
brandonyan <30865154+brandonyan@users.noreply.github.com>
2017-09-20 07:48:05 -0500

    Add files via upload

---
+++ compilador.py
@@ -0,0 +1,11 @@
+import re
+
+exp_valor = re.compile('[0-9]+$')
+exp_operador = re.compile('[\+\*\-/]{1,1}$')
+exp_variable = re.compile('([a-z]+)([A-Z0-9_]*)$')
+
+
+m = exp_variable.search('2casa')
+print m.group(0)
+
+print exp_variable.match("asa")
